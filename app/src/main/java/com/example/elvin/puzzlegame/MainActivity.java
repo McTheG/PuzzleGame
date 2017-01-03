e902b97e897cc7eb07f0290d8b42100aba055247
McTheG <freky_dewiek@hotmail.com>
2017-01-03 13:10:02 +0100

    X,Y and Z coordinate display

--- app/src/main/java/com/example/elvin/puzzlegame/MainActivity.java
+++ app/src/main/java/com/example/elvin/puzzlegame/MainActivity.java
@@ -1,16 +1,58 @@
 package com.example.elvin.puzzlegame;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView textX, textY, textZ;
+    private Sensor mySensor;
+    private SensorManager SensorManager;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create sensor manager
+        SensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer sensor
+        mySensor = SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register SensorListener
+        SensorManager.registerListener(this,mySensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign textviews
+        textX = (TextView) findViewById(R.id.textX);
+        textY = (TextView) findViewById(R.id.textY);
+        textZ = (TextView) findViewById(R.id.textZ);
     }
 
-    //Hello mijn jongen
-    //hello
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        textX.setText("X: " + event.values[0]);
+        textY.setText("Y: " + event.values[1]);
+        textZ.setText("Z: " + event.values[2]);
+    }
+
+    //gebruiken we niet, is voor error weg te halen als deze er niet is
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
