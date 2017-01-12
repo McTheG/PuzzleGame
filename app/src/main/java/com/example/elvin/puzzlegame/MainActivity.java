b3f56ccc0ed272836a8984d4255f1fe4d27fe89e
McTheG <freky_dewiek@hotmail.com>
2017-01-13 00:45:26 +0100

    X,Y and Z coordinate display + basic game demo

--- app/src/main/java/com/example/elvin/puzzlegame/MainActivity.java
+++ app/src/main/java/com/example/elvin/puzzlegame/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView textX, textY, textZ;
     private Sensor mySensor;
     private SensorManager SensorManager;
-
+    private ImageView lock1_imageView, lock2_imageView, lock3_imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textX = (TextView) findViewById(R.id.textX);
         textY = (TextView) findViewById(R.id.textY);
         textZ = (TextView) findViewById(R.id.textZ);
+        lock1_imageView = (ImageView) findViewById(R.id.lock1_imageView);
+        lock2_imageView = (ImageView) findViewById(R.id.lock2_imageView);
+        lock3_imageView = (ImageView) findViewById(R.id.lock3_imageView);
     }
 
     @Override
@@ -49,6 +53,70 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textX.setText("X: " + event.values[0]);
         textY.setText("Y: " + event.values[1]);
         textZ.setText("Z: " + event.values[2]);
+
+        //Postive Numbers
+        if(event.values[0] > 0 && event.values[0] < 1) {
+            lock1_imageView.setImageResource(R.drawable.number0);
+        }
+        if(event.values[0] > 1 && event.values[0] < 2) {
+            lock1_imageView.setImageResource(R.drawable.number1);
+        }
+        if(event.values[0] > 2 && event.values[0] < 3) {
+            lock1_imageView.setImageResource(R.drawable.number2);
+        }
+        if(event.values[0] > 3 && event.values[0] < 4) {
+            lock1_imageView.setImageResource(R.drawable.number3);
+        }
+        if(event.values[0] > 4 && event.values[0] < 5) {
+            lock1_imageView.setImageResource(R.drawable.number4);
+        }
+        if(event.values[0] > 5 && event.values[0] < 6) {
+            lock1_imageView.setImageResource(R.drawable.number5);
+        }
+        if(event.values[0] > 6 && event.values[0] < 7) {
+            lock1_imageView.setImageResource(R.drawable.number6);
+        }
+        if(event.values[0] > 7 && event.values[0] < 8) {
+            lock1_imageView.setImageResource(R.drawable.number7);
+        }
+        if(event.values[0] > 8 && event.values[0] < 9) {
+            lock1_imageView.setImageResource(R.drawable.number8);
+        }
+        if(event.values[0] > 9 && event.values[0] < 10) {
+            lock1_imageView.setImageResource(R.drawable.number9);
+        }
+
+        //Negative Numbers
+        if(event.values[0] < -0 && event.values[0] > -1) {
+            lock1_imageView.setImageResource(R.drawable.number0);
+        }
+        if(event.values[0] < -1 && event.values[0] > -2) {
+            lock1_imageView.setImageResource(R.drawable.number1);
+        }
+        if(event.values[0] < -2 && event.values[0] > -3) {
+            lock1_imageView.setImageResource(R.drawable.number2);
+        }
+        if(event.values[0] < -3 && event.values[0] > -4) {
+            lock1_imageView.setImageResource(R.drawable.number3);
+        }
+        if(event.values[0] < -4 && event.values[0] > -5) {
+            lock1_imageView.setImageResource(R.drawable.number4);
+        }
+        if(event.values[0] < -5 && event.values[0] > -6) {
+            lock1_imageView.setImageResource(R.drawable.number5);
+        }
+        if(event.values[0] < -6 && event.values[0] > -7) {
+            lock1_imageView.setImageResource(R.drawable.number6);
+        }
+        if(event.values[0] < -7 && event.values[0] > -8) {
+            lock1_imageView.setImageResource(R.drawable.number7);
+        }
+        if(event.values[0] < -8 && event.values[0] > -9) {
+            lock1_imageView.setImageResource(R.drawable.number8);
+        }
+        if(event.values[0] < -9 && event.values[0] > -10) {
+            lock1_imageView.setImageResource(R.drawable.number9);
+        }
     }
 
     //gebruiken we niet, is voor error weg te halen als deze er niet is
